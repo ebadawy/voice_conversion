3e884ddc724a93c4acfa89306d624b8cfb48da55
RussellSB <r.sammutbonnici@gmail.com>
2021-07-04 13:03:30 +0000

    Inference with input wav and gen id specification

--- src/inference.py
+++ src/inference.py
@@ -5,21 +5,24 @@ import itertools
 import sys
 from tqdm import tqdm
 
-# from torch.autograd import Variable
-# from models import *
-# import torch.nn as nn
-# import torch
-# import pickle
+from torch.autograd import Variable
+from models import *
+import torch.nn as nn
+import torch
+import pickle
 
 import librosa
-from utils import ls, preprocess_wav, melspectrogram, to_numpy
+from utils import ls, preprocess_wav, melspectrogram, to_numpy, plot_mel_transfer_infer, reconstruct_waveform
+from params import sample_rate
+import soundfile as sf
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epoch", type=int, default=0, help="saved version based on epoch to test from")
+parser.add_argument("--epoch", type=int, default=99, help="saved version based on epoch to test from")
 parser.add_argument("--model_name", type=str, help="name of the model")
 parser.add_argument("--gen_id", type=str, help="id of the generator for the target domain")
 parser.add_argument("--wav", type=str, help="path to wav file for input to transfer")
-parser.add_argument("--outdir", type=str, default='out_infer', help="path to output directory for the conversion")
+
+parser.add_argument("--plot", type=int, default=1, help="plot the spectrograms before and after (disable with -1)")
 parser.add_argument("--n_overlap", type=int, default=4, help="number of overlaps per slice")
 parser.add_argument("--img_height", type=int, default=128, help="size of image height")
 parser.add_argument("--img_width", type=int, default=128, help="size of image width")
@@ -30,7 +33,7 @@ parser.add_argument("--dim", type=int, default=32, help="number of filters in fi
 opt = parser.parse_args()
 print(opt)
 
-os.makedirs(opt.outdir, exist_ok=True)
+os.makedirs('out_infer', exist_ok=True)
 
 cuda = True if torch.cuda.is_available() else False
 Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
@@ -39,13 +42,13 @@ input_shape = (opt.channels, opt.img_height, opt.img_width)
 # Dimensionality (channel-wise) of image embedding
 shared_dim = opt.dim * 2 ** opt.n_downsample
 
-# Initialize generator and discriminator
+# Initialize encoder and decoder
 encoder = Encoder(dim=opt.dim, in_channels=opt.channels, n_downsample=opt.n_downsample)
 G = Generator(dim=opt.dim, out_channels=opt.channels, n_upsample=opt.n_downsample, shared_block=ResidualBlock(features=shared_dim))
 
 if cuda:
     encoder = encoder.cuda()
-    G = G1.cuda()
+    G = G.cuda()
 
 assert os.path.exists("saved_models/%s/encoder_%02d.pth" % (opt.model_name, opt.epoch)), 'Check that trained encoder exists'
 assert os.path.exists("saved_models/%s/G%s_%02d.pth" % (opt.model_name, opt.gen_id, opt.epoch)), 'Check that trained generator exists'
@@ -68,10 +71,13 @@ spect_src = melspectrogram(sample)
 
 def infer(S):
     """Takes in a standard sized spectrogram, returns converted version"""
+    S = torch.from_numpy(S)
+    S = S.view(1, 1, opt.img_height, opt.img_width)
+    
     X = Variable(S.type(Tensor))
     mu, Z = encoder(X)  # Get shared latent representation
     fake_X = G(Z)  # Translate speech
-    retrun to_numpy(fake_X)
+    return to_numpy(fake_X)
 
 # ------------------------------------------
 #  Global Inference (w/ a sliding window)
@@ -82,7 +88,7 @@ length = spect_src.shape[1]
 spect_trg = np.zeros(spect_src.shape)
 hop = opt.img_width // opt.n_overlap
 
-for i in range(0, length, hop):
+for i in tqdm(range(0, length, hop)):
     x = i + opt.img_width
          
     # Get cropped spectro of right dims
@@ -92,8 +98,7 @@ for i in range(0, length, hop):
         S = spect_src[:, i:]
         S = np.pad(S, ((0,0),(x-length,0)), 'constant') 
         
-    # Perform inference from trained model
-    T = infer(S)
+    T = infer(S) # perform inference from trained model
     
     # Add parts of target spectrogram with an average across overlapping segments    
     for j in range(0, opt.img_width, hop):
@@ -102,14 +107,22 @@ for i in range(0, length, hop):
         t = T[:, j:y]
         spect_trg[:, i+j:i+y] += t/opt.n_overlap  # add average element
 
-# Removing initial left pad
-spect_trg = spect_trg[:, opt.img_width:-opt.img_width]
+spect_trg = spect_trg[:, opt.img_width:-opt.img_width] # remove initial padding
 
+# prepare file name for saving
+f = opt.wav.split('/')[-1]
+wavname = f.split('.')[0]
+fname = 'G%s_%s_%s_%s' % (opt.gen_id, opt.model_name, opt.epoch, wavname)
 
-# progress = tqdm(enumerate(dataloader),desc='',total=len(dataloader))
-# for i, batch in progress:
+# plot transfer if specified
+if opt.plot != -1:
+    os.makedirs('out_infer/plots/', exist_ok=True)
+    spect_src = spect_src[:, opt.img_width:-opt.img_width]
+    plot_mel_transfer_infer('out_infer/plots/%s.png' % fname, spect_src, spect_trg)  
 
+# reconstruct with Griffin Lim (later feed this wav as input to vocoder)
+print('Reconstructing with Griffin Lim...')
+x = reconstruct_waveform(spect_trg)
 
-        
-# # Save converted output in pickle format
-# pickle.dump(feats,open('out_eval/%s/out_%s.pickle'%(opt.model_name, opt.epoch),'wb'))
+sf.write('out_infer/%s_gen.wav'%fname, x, sample_rate)  # generated output
+sf.write('out_infer/%s_ref.wav'%fname, sample, sample_rate)  # input reference (for convinience)
