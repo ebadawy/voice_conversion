e532213b0a66671aade314f0106d91e3e0258473
RussellSB <r.sammutbonnici@gmail.com>
2021-07-29 11:48:11 +0100

    Training for all cyclic combinations

--- src/train.py
+++ src/train.py
@@ -22,7 +22,7 @@ parser.add_argument("--epoch", type=int, default=0, help="epoch to start trainin
 parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
 parser.add_argument("--model_name", type=str, help="name of the model")
 parser.add_argument("--dataset", type=str, help="path to dataset for training")
-parser.add_argument("--n_spkrs", type=int, default=2, help="number of speakers for conversion")
+parser.add_argument("--n_spkrs", type=int, default=2, help="number of speakers for conversion, must match that of preprocessing")
 parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
@@ -45,10 +45,14 @@ cuda = True if torch.cuda.is_available() else False
 # Create sample and checkpoint directories
 os.makedirs("saved_models/%s" % opt.model_name, exist_ok=True)
 
+# finds all possible transfer pairings without repetition (each will be used for a forward and backward pass)
+transfer_combos = list(itertools.combinations(range(opt.n_spkrs), 2))
+
 # Create plot output directories
 if opt.plot_interval != -1:
-    os.makedirs("out_train/%s/plot_A2B/" % opt.model_name, exist_ok=True)
-    os.makedirs("out_train/%s/plot_B2A/" % opt.model_name, exist_ok=True)
+    for pair in transfer_combos:
+        os.makedirs("out_train/%s/plot_%d_%d/" % (opt.model_name, pair[0], pair[1]), exist_ok=True)
+        os.makedirs("out_train/%s/plot_%d_%d/" % (opt.model_name, pair[1], pair[0]), exist_ok=True)
 
 # Losses
 criterion_GAN = torch.nn.MSELoss()
@@ -59,37 +63,31 @@ input_shape = (opt.channels, opt.img_height, opt.img_width)
 # Dimensionality (channel-wise) of image embedding
 shared_dim = opt.dim * 2 ** opt.n_downsample
 
-# Initialize generator and discriminator
+# Initialize generators and discriminators
 encoder = Encoder(dim=opt.dim, in_channels=opt.channels, n_downsample=opt.n_downsample)
 shared_G = ResidualBlock(features=shared_dim)
-G1 = Generator(dim=opt.dim, out_channels=opt.channels, n_upsample=opt.n_downsample, shared_block=shared_G)
-G2 = Generator(dim=opt.dim, out_channels=opt.channels, n_upsample=opt.n_downsample, shared_block=shared_G)
-D1 = Discriminator(input_shape)
-D2 = Discriminator(input_shape)
+G = [Generator(dim=opt.dim, out_channels=opt.channels, n_upsample=opt.n_downsample, shared_block=shared_G) for _ in range(opt.n_spkrs)]
+D = [Discriminator(input_shape) for _ in range(opt.n_spkrs)]
 
 if cuda:
     encoder = encoder.cuda()
-    G1 = G1.cuda()
-    G2 = G2.cuda()
-    D1 = D1.cuda()
-    D2 = D2.cuda()
+    G = [G_n.cuda() for G_n in G]
+    D = [D_n.cuda() for D_n in D]
     criterion_GAN.cuda()
     criterion_pixel.cuda()
 
 if opt.epoch != 0:
     # Load pretrained models
     encoder.load_state_dict(torch.load("saved_models/%s/encoder_%02d.pth" % (opt.model_name, opt.epoch)))
-    G1.load_state_dict(torch.load("saved_models/%s/G1_%02d.pth" % (opt.model_name, opt.epoch)))
-    G2.load_state_dict(torch.load("saved_models/%s/G2_%02d.pth" % (opt.model_name, opt.epoch)))
-    D1.load_state_dict(torch.load("saved_models/%s/D1_%02d.pth" % (opt.model_name, opt.epoch)))
-    D2.load_state_dict(torch.load("saved_models/%s/D2_%02d.pth" % (opt.model_name, opt.epoch)))
+    for n in range(opt.n_spkrs):
+        G[n].load_state_dict(torch.load("saved_models/%s/G%d_%02d.pth" % (opt.model_name, n+1, opt.epoch)))
+        D[n].load_state_dict(torch.load("saved_models/%s/D%d_%02d.pth" % (opt.model_name, n+1, opt.epoch)))
 else:
     # Initialize weights
     encoder.apply(weights_init_normal)
-    G1.apply(weights_init_normal)
-    G2.apply(weights_init_normal)
-    D1.apply(weights_init_normal)
-    D2.apply(weights_init_normal)
+    for n in range(opt.n_spkrs):
+        G[n].apply(weights_init_normal)
+        D[n].apply(weights_init_normal)
 
 # Loss weights
 lambda_0 = 10   # GAN
@@ -101,23 +99,19 @@ lambda_5 = 10   # latent space L1
 
 # Optimizers
 optimizer_G = torch.optim.Adam(
-    itertools.chain(encoder.parameters(), G1.parameters(), G2.parameters()),
+    itertools.chain(encoder.parameters(), *[G_n.parameters() for G_n in G]),
     lr=opt.lr,
     betas=(opt.b1, opt.b2),
 )
-optimizer_D1 = torch.optim.Adam(D1.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D2 = torch.optim.Adam(D2.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = [torch.optim.Adam(D_n.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2)) for D_n in D]
 
 # Learning rate update schedulers
 lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(
     optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step
 )
-lr_scheduler_D1 = torch.optim.lr_scheduler.LambdaLR(
-    optimizer_D1, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step
-)
-lr_scheduler_D2 = torch.optim.lr_scheduler.LambdaLR(
-    optimizer_D2, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step
-)
+lr_scheduler_D = [torch.optim.lr_scheduler.LambdaLR(
+                    optimizer_D_n, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step
+                ) for optimizer_D_n in optimizer_D]
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
 
@@ -136,22 +130,18 @@ def compute_kl(mu):
     return loss
 
 
-# ----------
-#  Training
-# ----------
+# ---------------------------------------------------------
+#  Training (local)
+# ---------------------------------------------------------
 
-for epoch in range(opt.epoch, opt.n_epochs):
-    losses = {'G': [],'D': []}
-    progress = tqdm(enumerate(dataloader),desc='',total=len(dataloader))
-    for i, batch in progress:
-
+def train_local(i, epoch, batch, id_1, id_2, losses):
         # Set model input
-        X1 = Variable(batch["A"].type(Tensor))
-        X2 = Variable(batch["B"].type(Tensor))
+        X1 = Variable(batch[id_1].type(Tensor))
+        X2 = Variable(batch[id_2].type(Tensor))
 
         # Adversarial ground truths
-        valid = Variable(Tensor(np.ones((X1.size(0), *D1.output_shape))), requires_grad=False)
-        fake = Variable(Tensor(np.zeros((X1.size(0), *D1.output_shape))), requires_grad=False)
+        valid = Variable(Tensor(np.ones((X1.size(0), *D[0].output_shape))), requires_grad=False)
+        fake = Variable(Tensor(np.zeros((X1.size(0), *D[0].output_shape))), requires_grad=False)
 
         # -------------------------------
         #  Train Encoder and Generators
@@ -168,22 +158,22 @@ for epoch in range(opt.epoch, opt.n_epochs):
         feat_2 = mu2.view(mu2.size()[0], -1).mean(dim=0)
 
         # Reconstruct speech
-        recon_X1 = G1(Z1)
-        recon_X2 = G2(Z2)
+        recon_X1 = G[id_1](Z1)
+        recon_X2 = G[id_2](Z2)
 
         # Translate speech
-        fake_X1 = G1(Z2)
-        fake_X2 = G2(Z1)
+        fake_X1 = G[id_1](Z2)
+        fake_X2 = G[id_2](Z1)
 
         # Cycle translation
         mu1_, Z1_ = encoder(fake_X1)
         mu2_, Z2_ = encoder(fake_X2)
-        cycle_X1 = G1(Z2_)
-        cycle_X2 = G2(Z1_)
+        cycle_X1 = G[id_1](Z2_)
+        cycle_X2 = G[id_2](Z1_)
 
         # Losses
-        loss_GAN_1 = lambda_0 * criterion_GAN(D1(fake_X1), valid)
-        loss_GAN_2 = lambda_0 * criterion_GAN(D2(fake_X2), valid)
+        loss_GAN_1 = lambda_0 * criterion_GAN(D[id_1](fake_X1), valid)
+        loss_GAN_2 = lambda_0 * criterion_GAN(D[id_2](fake_X2), valid)
         loss_KL_1 = lambda_1 * compute_kl(mu1)
         loss_KL_2 = lambda_1 * compute_kl(mu2)
         loss_ID_1 = lambda_2 * criterion_pixel(recon_X1, X1)
@@ -216,49 +206,70 @@ for epoch in range(opt.epoch, opt.n_epochs):
         #  Train Discriminator 1
         # -----------------------
 
-        optimizer_D1.zero_grad()
+        optimizer_D[id_1].zero_grad()
 
-        loss_D1 = criterion_GAN(D1(X1), valid) + criterion_GAN(D1(fake_X1.detach()), fake)
+        loss_D1 = criterion_GAN(D[id_1](X1), valid) + criterion_GAN(D[id_1](fake_X1.detach()), fake)
 
         loss_D1.backward()
-        optimizer_D1.step()
+        optimizer_D[id_1].step()
 
         # -----------------------
         #  Train Discriminator 2
         # -----------------------
 
-        optimizer_D2.zero_grad()
+        optimizer_D[id_2].zero_grad()
 
-        loss_D2 = criterion_GAN(D2(X2), valid) + criterion_GAN(D2(fake_X2.detach()), fake)
+        loss_D2 = criterion_GAN(D[id_2](X2), valid) + criterion_GAN(D[id_2](fake_X2.detach()), fake)
 
         loss_D2.backward()
-        optimizer_D2.step()
+        optimizer_D[id_2].step()
 
         # --------------
         #  Log Progress
         # --------------
+        
+        # Plot first batch every epoch or few epochs
+        if opt.plot_interval != -1 and (epoch+1) % opt.plot_interval == 0 and i == 0:
+            plot_batch_train(opt.model_name, 'plot_%d_%d'%(id_1, id_2), epoch, X1, cycle_X1, fake_X2, X2)
+            plot_batch_train(opt.model_name, 'plot_%d_%d'%(id_2, id_1), epoch, X2, cycle_X2, fake_X1, X1)
 
         losses['G'].append(loss_G.item())
         losses['D'].append((loss_D1 + loss_D2).item())
 
-        # Update progress bar
-        progress.set_description("[Epoch %d/%d] [D loss: %f] [G loss: %f] "
-            % (epoch,opt.n_epochs,np.mean(losses['D']), np.mean(losses['G'])))
+        return losses
+
+
+# ---------------------------------------------------------
+#  Training (global)
+# ---------------------------------------------------------
+
+def train_global():
+    for epoch in range(opt.epoch, opt.n_epochs):
+
+        losses = {'G': [],'D': []}
+        progress = tqdm(enumerate(dataloader),desc='',total=len(dataloader))
         
-        # Plot first batch every epoch or few epochs
-        if opt.plot_interval != -1 and epoch % opt.plot_interval == 0 and i == 0:
-            plot_batch_train(opt.model_name, 'plot_A2B', epoch, X1, cycle_X1, fake_X2, X2)
-            plot_batch_train(opt.model_name, 'plot_B2A', epoch, X2, cycle_X2, fake_X1, X1)
+        for i, batch in progress:
 
-    # Update learning rates
-    lr_scheduler_G.step()
-    lr_scheduler_D1.step()
-    lr_scheduler_D2.step()
+            for pair in transfer_combos:         
+                losses = train_local(i, epoch, batch, pair[0], pair[1], losses)
 
-    if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
-        # Save model checkpoints
-        torch.save(encoder.state_dict(), "saved_models/%s/encoder_%02d.pth" % (opt.model_name, epoch))
-        torch.save(G1.state_dict(), "saved_models/%s/G1_%02d.pth" % (opt.model_name, epoch))
-        torch.save(G2.state_dict(), "saved_models/%s/G2_%02d.pth" % (opt.model_name, epoch))
-        torch.save(D1.state_dict(), "saved_models/%s/D1_%02d.pth" % (opt.model_name, epoch))
-        torch.save(D2.state_dict(), "saved_models/%s/D2_%02d.pth" % (opt.model_name, epoch))
+            # Update progress bar
+            progress.set_description("[Epoch %d/%d] [D loss: %f] [G loss: %f] "
+                % (epoch, opt.n_epochs, np.mean(losses['D']), np.mean(losses['G'])))
+
+        # Update learning rates
+        lr_scheduler_G.step()
+        for n in range(0, opt.n_spkrs):
+            lr_scheduler_D[n].step()
+
+        if opt.checkpoint_interval != -1 and (epoch+1) % opt.checkpoint_interval == 0:
+            # Save model checkpoints
+            torch.save(encoder.state_dict(), "saved_models/%s/encoder_%02d.pth" % (opt.model_name, epoch))
+            for n in range(0, opt.n_spkrs):
+                torch.save(G[n].state_dict(), "saved_models/%s/G%d_%02d.pth" % (opt.model_name, n+1, epoch))
+                torch.save(D[n].state_dict(), "saved_models/%s/D%d_%02d.pth" % (opt.model_name, n+1, epoch))
+
+
+if __name__ == '__main__':
+    train_global()
